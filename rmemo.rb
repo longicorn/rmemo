ab43a770f0dff1c056703a17fc7a194795fe01b9
longicorn <longicorn.c@gmail.com>
2015-04-17 14:24:47 +0900

    version up

--- rmemo.rb
+++ rmemo.rb
@@ -13,7 +13,7 @@ require 'fileutils'
 require 'tempfile'
 
 #------- global variable
-Version = "0.0.9.4"
+Version = "0.0.9.5"
 MemoDir = File.expand_path('~/.rmemo')
 Editor = 'vim'
 #------- global variable
